5b885fc6ec3b650895fe95cc42d73018668d6f0c
B <henryirons@yahoo.com>
2020-08-12 22:22:42 +0100

    added state to app.tsx and used the properties in the state as props for the QuestionCard component

--- quiz/react-quiz/src/App.tsx
+++ quiz/react-quiz/src/App.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 //components
 import QuestionCard from "./components/QuestionCard";
+
+const TOTAL_QUESTIONS = 10;
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setquestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
   const startTrivia = async () => {};
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
@@ -16,7 +25,14 @@ const App = () => {
       </button>
       <p className="score">score:</p>
       <p>Loading Questions...</p>
-      <QuestionCard />
+      <QuestionCard
+        question={questions[number].question}
+        answers={questions[number].answers}
+        callback={checkAnswer}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        questionNr={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+      />
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
