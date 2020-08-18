f49a420eeeff2a7bb4f15634c85e68b6c9af3e82
B <henryirons@yahoo.com>
2020-08-18 23:51:44 +0100

    created utils.ts imported the shuffleArray function in API.ts

--- quiz/react-quiz/src/API.ts
+++ quiz/react-quiz/src/API.ts
@@ -1,3 +1,6 @@
+
+
+import {shuffleArray} from './utils'
 export enum Difficulty {
   EASY = "easy",
   MEDIUM = "medium",
@@ -20,5 +23,7 @@ export const fetchQuizQuestions = async (
 ) => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   const data = await (await fetch(endpoint)).json();
-  console.log(data);
+  return data.results.map((question:Question)=>{
+    ...question,answer:shuffleArray([question.incorrect_answer,question.correct_answer])
+  })
 };
